8f15f7c9cdcbcfee8c3de27588b8d5bedecc6f75
Javier Montemayor <jmontemayorm@users.noreply.github.com>
2018-04-19 15:02:07 +0200

    Fixed compatibility issues
    The function now reads the faces regardless of if they include information about the texture or normals (that information is ignored).

--- open_obj.m
+++ open_obj.m
@@ -2,9 +2,9 @@ function [vertices,faces] = open_obj(FILENAME)
     %OPEN_OBJ   Reads the data from a .obj file.
     %   Opens and reads a .obj (Wavefront) file, returning the vertices and
     %   faces. It is only compatible with objects created out of triangluar
-    %   faces and that do not contain a w value in the vertices. Also
-    %   incompatible with objects that contain texture information in the
-    %   faces. For more information on .obj files see:
+    %   faces and that do not contain a w value in the vertices. It ignores
+    %   any texture information (vertex texture coordinates) and vertex
+    %   normal indices. For more information on .obj files see:
     %   https://en.wikipedia.org/wiki/Wavefront_.obj_file
     %
     %   OPEN_OBJ(FILENAME) returns the [vertices,faces] described by the
@@ -32,9 +32,40 @@ function [vertices,faces] = open_obj(FILENAME)
                 vIdx = cell2mat(data{1}) == 'v';
                 fIdx = cell2mat(data{1}) == 'f';
                 
-                % Extracts vertices and faces
+                % Extracts the vertices
                 vertices = [data{2}(vIdx) data{3}(vIdx) data{4}(vIdx)];
-                faces = [data{2}(fIdx) data{3}(fIdx) data{4}(fIdx)];
+                
+                % Analyzes the type of faces and extracts them
+                k = find(fIdx,1);
+                if ~isempty(data{3}(k))
+                    % Faces are of the form f v1 v2 v3
+                    faces = [data{2}(fIdx) data{3}(fIdx) data{4}(fIdx)];
+                else
+                    data = textscan(fID,'%s %f/%f %f/%f %f/%f','CommentStyle','#');
+                    
+                    if ~isempty(data{4}(k))
+                        % Faces are of the form f v1/vt1 v2/vt2 v3/vt3
+                        faces = [data{2}(fIdx) data{4}(fIdx) data{6}(fIdx)];
+                    else
+                        data = textscan(fID,'%s %f//%f %f//%f %f//%f','CommentStyle','#');
+                        
+                        if ~isempty(data{3}(k))
+                            % Faces are of the form f v1//vn1 v2//vn2 v3//vn3
+                            faces = [data{2}(fIdx) data{4}(fIdx) data{6}(fIdx)];
+                        else
+                            data = textscan(fID,'%s %f/%f/%f %f/%f/%f %f/%f/%f','CommentStyle','#');
+                            
+                            if ~isempty(data{5}(k))
+                                % Faces are of the form f v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3
+                                faces = [data{2}(fIdx) data{5}(fIdx) data{8}(fIdx)];
+                            else
+                                % Closes the file and notifies the error
+                                fclose(fID);
+                                error('There was a problem reading the faces of the object.');
+                            end
+                        end
+                    end
+                end
                 
                 % Closes the file
                 fclose(fID);
