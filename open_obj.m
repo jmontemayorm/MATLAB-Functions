6179d0e8d281a5cedf820742fa1754fc9927f264
Javier Montemayor <jmontemayorm@users.noreply.github.com>
2018-04-19 14:38:12 +0200

    Speed improvements
    Files are now read faster. Files with texture information in the faces are not compatible with this fucntion.

--- open_obj.m
+++ open_obj.m
@@ -1,84 +1,43 @@
-function [vertices,faces] = open_obj(fileName)
-    %OPEN_OBJ Opens and reads the .obj (Wavefront) file, returning
-    %   the vertices and faces. Does not make use of the associated
-    %   .mlt and .png files, nor does it read data other than what
-    %   it returns.
-    %   This function is currently compatible only with triangular
-    %   faces (3 vertices per face).
+function [vertices,faces] = open_obj(FILENAME)
+    %OPEN_OBJ   Reads the data from a .obj file.
+    %   Opens and reads a .obj (Wavefront) file, returning the vertices and
+    %   faces. It is only compatible with objects created out of triangluar
+    %   faces and that do not contain a w value in the vertices. Also
+    %   incompatible with objects that contain texture information in the
+    %   faces. For more information on .obj files see:
+    %   https://en.wikipedia.org/wiki/Wavefront_.obj_file
+    %
+    %   OPEN_OBJ(FILENAME) returns the [vertices,faces] described by the
+    %   .obj file called FILENAME. If the file is inside the working
+    %   directory, no path is required, if it's not, the path to the file
+    %   must be included in FILENAME.
     
     %   Author: Javier Montemayor Mancias
     %   Created on: 2018.03.19
     %   Last updated: 2018.03.19
-    %   Version: v1.0
+    %   Version: v1.1
     
-    % Checks if the number of arguments match the function
+    % Checks for the amount of input arguments and for the existence of the
+    % file. Attempts to read. Throws error message or returns the read
+    % data.
     if nargin == 1
-        % Checks if the file exists
-        if exist(fileName,'file')
+        if exist(FILENAME,'file')
             % Opens the file and checks if there were any errors
-            fileID = fopen(fileName);
-            if fileID >= 3
-                % Initializes vectors to store the lines with useful data
-                verticesLines(1) = "";
-                facesLines(1) = "";
+            fID = fopen(FILENAME);
+            if fID >= 3
+                % Gets the data
+                data = textscan(fID,'%s %f %f %f','CommentStyle','#');
                 
-                % Initializes counters to keep track of the arrays
-                countV = 0;
-                countF = 0;
+                % Gets the indices of vertices and faces
+                vIdx = cell2mat(data{1}) == 'v';
+                fIdx = cell2mat(data{1}) == 'f';
                 
-                % Gets the first line of the file
-                tline = fgetl(fileID);
-                
-                % Loops through the file extracting raw data
-                while ischar(tline)
-                    % Checks if the line is not empty
-                    if ~isempty(tline)
-                        % Checks if the current line has vertices or faces
-                        if all(tline(1:2) == 'v ')
-                            countV = countV + 1;
-                            verticesLines(countV,1) = strrep(tline,'v ','');
-                        elseif all(tline(1:2) == 'f ')
-                            countF = countF + 1;
-                            facesLines(countF,1) = strrep(tline,'f ','');
-                        end
-                    end
-                    
-                    % Goes to the next line
-                    tline = fgetl(fileID);
-                end
-                
-                % Allocates space for the string extraction
-                strV = strings(countV,3);
-                strF = strings(countF,3);
-                
-                % Extracts the vertices into the return matrix
-                strV(1:countV,1) = extractBefore(...
-                    verticesLines(1:countV),' ');
-                strV(1:countV,2) = extractBetween(...
-                    verticesLines(1:countV),' ',' ');
-                strV(1:countV,3) = extractAfter(...
-                    verticesLines(1:countV),strV(1:countV,2));
-                vertices = str2double(strV);
-                
-                % Extracts the faces into the return matrix, checking if
-                % there are '/' in the string so it can ignore them
-                if ~contains(extractBefore(facesLines(1),' '),'/')
-                    strF(1:countF,1) = extractBefore(...
-                        facesLines(1:countF),' ');
-                    strF(1:countF,2) = extractBetween(...
-                        facesLines(1:countF),' ',' ');
-                    strF(1:countF,3) = extractAfter(...
-                        facesLines(1:countF),strF(1:countF,2));
-                else
-                    strF(1:countF,1) = extractBefore(...
-                        facesLines(1:countF),'/');
-                    strF(1:countF,2:3) = extractBetween(...
-                        facesLines(1:countF),' ','/');
-                end
-                faces = str2double(strF);
+                % Extracts vertices and faces
+                vertices = [data{2}(vIdx) data{3}(vIdx) data{4}(vIdx)];
+                faces = [data{2}(fIdx) data{3}(fIdx) data{4}(fIdx)];
                 
                 % Closes the file
-                fclose(fileID);
+                fclose(fID);
             else
                 error('There was an error opening the file.');
             end
@@ -86,6 +45,6 @@ function [vertices,faces] = open_obj(fileName)
             error('File not found.');
         end
     else
-        error('The number of arguments does not match this function.');
+        error('The amount of input arguments does not match this function.');
     end
 end
